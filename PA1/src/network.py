8faee6bbc71fa5b4842337832496eb7ffbbe5d73
HAHAO <168169221+MsDobby@users.noreply.github.com>
2024-05-10 22:03:04 +0900

    Update network.py

--- PA1/src/network.py
+++ PA1/src/network.py
@@ -9,6 +9,7 @@ from lightning.pytorch.loggers.wandb import WandbLogger
 from torch import nn
 from torchvision import models
 from torchvision.models.alexnet import AlexNet
+
 import torch
 
 # Custom packages
@@ -16,27 +17,257 @@ from src.metric import MyAccuracy, MyF1Score
 import src.config as cfg
 from src.util import show_setting
 
-
-# [TODO: Optional] Rewrite this class if you want
-class MyNetwork(AlexNet):
-    def __init__(self):
+#############################################################################################
+# My Extractor (deprecated) 
+class ICLR2015(AlexNet):
+    def __init__(self,num_classes,dropout):
         super().__init__()
-
+        print("****************** My Feature Extractor ******************")
         # [TODO] Modify feature extractor part in AlexNet
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=5),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(96, 96, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(96, 96, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+
+            nn.Conv2d(384, 1024, kernel_size=1, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(1024, 1024, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(1024, num_classes, kernel_size=1, stride=1),
+            nn.ReLU(inplace=True),
+        )
         
+        self.global_avgpooling = nn.AdaptiveAvgPool2d((6,6)) # global average pooling 
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # [TODO: Optional] Modify this as well if you want
         x = self.features(x)
+        x = self.global_avgpooling(x)
+        x = torch.flatten(x, 1)
+        # x =  torch.nn.functional.softmax(x, dim=1)
+        return x
+#############################################################################################
+# AlexNet + GAP 
+class AlexNet_with_GAP(AlexNet):
+    def __init__(self,num_classes,dropout):
+        super().__init__()
+        # [TODO] Modify feature extractor part in AlexNet
+        self.global_avgpooling = nn.AdaptiveAvgPool2d((1,1)) # global average pooling 
+        self.fc = nn.Linear(256,num_classes)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # [TODO: Optional] Modify this as well if you want
+        x = self.features(x)
+        x = self.global_avgpooling(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+#############################################################################################
+# VGG16 (deprecated)
+def conv_2_block(in_dim,out_dim):
+    model = nn.Sequential(
+        nn.Conv2d(in_dim,out_dim,kernel_size=3,padding=1),
+        nn.ReLU(),
+        nn.Conv2d(out_dim,out_dim,kernel_size=3,padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2,2)
+    )
+    return model
+def conv_3_block(in_dim,out_dim):
+    model = nn.Sequential(
+        nn.Conv2d(in_dim,out_dim,kernel_size=3,padding=1),
+        nn.ReLU(),
+        nn.Conv2d(out_dim,out_dim,kernel_size=3,padding=1),
+        nn.ReLU(),
+        nn.Conv2d(out_dim,out_dim,kernel_size=3,padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2,2)
+    )
+    return model
+class vgg_like_network(AlexNet):
+    def __init__(self,num_classes,dropout):
+        # [TODO] Modify feature extractor part in AlexNet
+        print("****************** My Feature Extractor ******************")
+        super().__init__()
+
+        self.features = nn.Sequential(
+            conv_2_block(3,64), 
+            conv_2_block(64,128), 
+            conv_3_block(128,256),
+            conv_3_block(256,512), 
+            conv_3_block(512,512),       
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc_layer = nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 1000),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(1000, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
-        x = self.classifier(x)
+        x = self.fc_layer(x)
+        
+        return x 
+#############################################################################################        
+# [TODO] My Extractor   
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = nn.ReLU()(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = nn.ReLU()(out)
+        return out
+        
+class MyNetwork(nn.Module):
+    def __init__(self,num_classes,dropout):
+        # [TODO] Modify feature extractor part in AlexNet
+        super().__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.features = nn.Sequential(
+            self._make_layer(BasicBlock, 64, 2, stride=1),
+            self._make_layer(BasicBlock, 128, 2, stride=2),
+            self._make_layer(BasicBlock, 256, 2, stride=2),
+            self._make_layer(BasicBlock, 512, 2, stride=2),
+        )
+
+        self.linear = nn.Linear(512*BasicBlock.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = nn.ReLU()(self.bn1(self.conv1(x)))
+        x = self.features(x)
+        x = nn.AdaptiveAvgPool2d((1, 1))(x)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
         return x
+
+#############################################################################################
+# [TODO] Toward SOTA : ResNext-18
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, cardinality, stride=1):
+        super().__init__()
+        group_width = planes // cardinality
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, groups=cardinality, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes * self.expansion:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, planes * self.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * self.expansion)
+            )
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = self.relu(out)
+        return out
+
+class ResNeXt_50(nn.Module):
+    def __init__(self, num_classes, cardinality, dropout):
+        super().__init__()
+        self.cardinality = cardinality
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(64, 3, stride=1)
+        self.layer2 = self._make_layer(128, 4, stride=2)
+        self.layer3 = self._make_layer(256, 6, stride=2)
+        self.layer4 = self._make_layer(512, 3, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * Bottleneck.expansion, num_classes)
+
+    def _make_layer(self, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(Bottleneck(self.in_planes, planes, self.cardinality, stride))
+            self.in_planes = planes * Bottleneck.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.maxpool(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        return out
+
+
+#############################################################################################
 
 
 class SimpleClassifier(LightningModule):
     def __init__(self,
-                 model_name: str = 'resnet18',
+                 model_name: str = 'alexnet',
                  num_classes: int = 200,
                  optimizer_params: Dict = dict(),
                  scheduler_params: Dict = dict(),
@@ -45,8 +276,13 @@ class SimpleClassifier(LightningModule):
 
         # Network
         if model_name == 'MyNetwork':
-            self.model = MyNetwork()
+            print("********** Train MyNetwork **********")
+            self.model = MyNetwork(cfg.NUM_CLASSES, cfg.DROPOUT_RATE)
+        elif model_name == "SOTA":
+            print("********** Train SOTA **********")
+            self.model = ResNeXt_50(cfg.NUM_CLASSES, cfg.CARDINALITY, cfg.DROPOUT_RATE)
         else:
+            print("********** Train Baseline : {} **********".format(model_name))
             models_list = models.list_models()
             assert model_name in models_list, f'Unknown model name: {model_name}. Choose one from {", ".join(models_list)}'
             self.model = models.get_model(model_name, num_classes=num_classes)
